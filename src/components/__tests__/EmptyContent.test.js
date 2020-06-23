4d1291162b7bed0e9177af546831aa9b3aa2f70f
Lena <tsybulenko.helen@gmail.com>
2020-06-23 23:41:53 +0300

    gh-pages update

--- src/components/__tests__/EmptyContent.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import EmptyContent from '../EmptyContent';
-import renderer from 'react-test-renderer';
-
-it('Empty content', () => {
-  const EmptyContentSnapshot = renderer.create(<EmptyContent />).toJSON();
-  expect(EmptyContentSnapshot).toMatchSnapshot();
-});
